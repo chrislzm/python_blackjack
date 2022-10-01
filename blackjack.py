b0647c13c04ca82f8f721278c7bf3a4edc25b511
Chris Leung <me@chrisleung.com>
2022-09-30 21:09:30 -0400

    Add ability to define # players and enter player details

--- blackjack.py
+++ blackjack.py
@@ -15,6 +15,21 @@ def play_again():
         choice = input("Would you like to play again? (Y/N): ").capitalize()
     return choice == 'Y'
 
+def get_num_players():
+    choice = ''
+    while not choice.isdigit():
+        choice = input("How many players? ")
+    return int(choice)
+
+def get_player_details(player_number):
+    name = input(f"Player {player_number}, enter your name: ")
+    money = 0
+    while True:
+        choice = input(f"{name}, enter your starting money: ")
+        if choice.isdigit() and int(choice) > 0:
+            money = int(choice)
+            return (name,money)
+
 def has_money_to_play(players):
     for player in players:
         if player.money != 0:
@@ -41,21 +56,26 @@ class Player:
 
 while True:
     # Game setup
+    dealer = Player('Dealer', 0)
+    players = []
 
     # Ask for the number of players
+    num_players = get_num_players()
+    for player_num in range(1,num_players+1):
+        (name, money) = get_player_details(player_num)
+        players.append(Player(name,money))
+
     # Ask for the starting amount of money
     # Ask for the number of decks
 
-    dealer = Player('Dealer', 0)
-    players = [Player('Chris', 0)]
     game_on = True
 
     while game_on:
         # Print players and amounts
+
         # Deal to all players
         # Show dealer's 2nd card
         # For each player
-
 
         if not has_money_to_play(players):
             game_on = False
