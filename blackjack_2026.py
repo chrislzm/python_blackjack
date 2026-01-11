4bd4af9f6b85bdfba84be4a227250f158c448051
Chris Leung <25468399+chrislzm@users.noreply.github.com>
2026-01-10 21:38:14 -0800

    Added Player class, game setup and helper functions

--- blackjack_2026.py
+++ blackjack_2026.py
@@ -12,7 +12,7 @@ CARD_SUIT_SYMBOLS = {'Hearts': '♥', 'Clubs': '♣', 'Diamonds': '♦',
 CARD_RANKS = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
 CARD_RANK_VALUES = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8,
                     '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 1}
-PLAYER_BANK_START = 500
+PLAYER_STARTING_BANK = 500
 MINIMUM_BET = 15
 NUM_SHOE_DECKS = 6
 SHOE_CUT_CARD_POSITION = 52  # Reshuffle when one deck remains in the shoe
@@ -99,3 +99,65 @@ class Dealer():
             self.discard.clear()
             random.shuffle(self.shoe)
             self.drew_cut_card = False
+
+
+class Player():
+
+    def __init__(self, number, name, bank):
+        self.number = number
+        self.name = name
+        self.bank = bank
+        self.bet = 0
+        self.hand = Hand()
+
+
+def clear_screen() -> None:
+    """
+    Clears the display to a blank screen.
+    """
+    print('\n'*100)
+
+
+def get_num_players() -> int:
+    while True:
+        try:
+            num_players = int(input("Please enter number of players: "))
+        except ValueError:
+            print("Sorry, that's not a valid input.")
+        else:
+            if num_players > 0:
+                return num_players
+            print("Please enter a number greater than 0.")
+
+
+def get_player_name(player_num) -> str:
+    while True:
+        player_name = input(f"Player {player_num} - Please enter your name: ")
+        if len(player_name) > 0:
+            return player_name
+
+
+def main():
+    clear_screen()
+    print("Welcome to Blackjack!")
+    players = []
+    num_players = get_num_players()
+
+    for player_num in range(1, num_players+1):
+        player_name = get_player_name(player_num)
+        players.append(Player(player_num, player_name, PLAYER_STARTING_BANK))
+        print(f"Welcome, {player_name}!")
+
+    dealer = Dealer(NUM_SHOE_DECKS, SHOE_CUT_CARD_POSITION)
+
+    # Print rules
+    print("-------------------------- HOUSE RULES --------------------------\n"
+          f"All players start with ${PLAYER_STARTING_BANK}.\n"
+          f"Dealer must hit on soft 17.\n"
+          f"Shoe contains {NUM_SHOE_DECKS} decks.\n"
+          f"Shoe is reshuffled when less than {SHOE_CUT_CARD_POSITION} cards "
+          "remain in the shoe.")
+
+
+if __name__ == '__main__':
+    main()
