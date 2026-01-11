515b0db827d0c812ac03e75df130f3fe78378080
Chris Leung <25468399+chrislzm@users.noreply.github.com>
2026-01-10 20:08:07 -0800

    Add string representation tests

--- test_blackjack_2026.py
+++ test_blackjack_2026.py
@@ -11,6 +11,30 @@ from blackjack_2026 import Hand
 
 class TestBlackjack2026(unittest.TestCase):
 
+    '''
+    String tests
+    '''
+
+    def test_card_str(self):
+        card = Card("2", "Spades")
+        self.assertEqual(str(card), "[  ]")
+        card.face_up = True
+        self.assertEqual(str(card), "[2♠]")
+
+    def test_hand_str(self):
+        hand = Hand()
+        hand.cards.append(Card('10', 'Spades'))
+        hand.cards.append(Card('10', 'Hearts'))
+        hand.cards.append(Card('3', 'Clubs'))
+        self.assertEqual(str(hand), "[  ] [  ] [  ] ")
+        hand.cards[1].face_up = True
+        hand.cards[2].face_up = True
+        self.assertEqual(str(hand), "[  ] [10♥] [3♣] ")
+
+    '''
+    Hand Value tests
+    '''
+
     def test_hand_value_and_bust_false(self):
         hand = Hand()
         hand.cards.append(Card('10', 'Spades'))
